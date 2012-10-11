99f0afd3c661894b5d65289a21f920d31801d578
Daniel Heinrich <dannynullzwo@gmail.com>
2012-10-11 13:07:02 +0200

    github initial push

--- core/src/main/java/de/hofuniversity/iws/core/WebApp.java
+++ core/src/main/java/de/hofuniversity/iws/core/WebApp.java
@@ -1,31 +1,43 @@
 package de.hofuniversity.iws.core;
 
-import static playn.core.PlayN.*;
+import playn.core.*;
 
-import playn.core.Game;
-import playn.core.Image;
-import playn.core.ImageLayer;
+import static playn.core.PlayN.assets;
+import static playn.core.PlayN.graphics;
+
+import org.netbeans.saas.twitter.TwitterWhatAreYouDoingService;
+import org.netbeans.saas.RestResponse;
 
 public class WebApp implements Game {
-  @Override
-  public void init() {
-    // create and add background image layer
-    Image bgImage = assets().getImage("images/bg.png");
-    ImageLayer bgLayer = graphics().createImageLayer(bgImage);
-    graphics().rootLayer().add(bgLayer);
-  }
 
-  @Override
-  public void paint(float alpha) {
-    // the background automatically paints itself, so no need to do anything here!
-  }
+    @Override
+    public void init() {
+        // create and add background image layer
+        GroupLayer group = graphics().createGroupLayer();
 
-  @Override
-  public void update(float delta) {
-  }
+        Image bgImage = assets().getImage("images/bg.png");
 
-  @Override
-  public int updateRate() {
-    return 25;
-  }
+        ImageLayer bgLayer = graphics().createImageLayer(bgImage);
+        group.add(bgLayer);
+
+        CanvasImage img = graphics().createImage(128, 64);
+        img.canvas().fillCircle(64, 32, 32);
+        group.add(graphics().createImageLayer(img));
+
+        graphics().rootLayer().add(group);
+    }
+
+    @Override
+    public void paint(float alpha) {
+        // the background automatically paints itself, so no need to do anything here!
+    }
+
+    @Override
+    public void update(float delta) {
+    }
+
+    @Override
+    public int updateRate() {
+        return 25;
+    }
 }
