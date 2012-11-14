7ec88914feef691ea7ed56205e5c635d824252c3
Roboter Marvin <td@gmx.de>
2012-11-14 10:28:51 +0100

    jetzt ja

--- html/src/main/java/de/hofuniversity/iws/html/WebAppHtml.java
+++ html/src/main/java/de/hofuniversity/iws/html/WebAppHtml.java
@@ -1,18 +1,16 @@
 package de.hofuniversity.iws.html;
 
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import playn.core.PlayN;
-import playn.html.HtmlGame;
-import playn.html.HtmlPlatform;
 
-public class WebAppHtml extends HtmlGame {
+public class WebAppHtml implements EntryPoint {
 
     private final WebAppInjector injector = GWT.create(WebAppInjector.class);
-
+    
     @Override
-    public void start() {
-        HtmlPlatform platform = HtmlPlatform.register();
-        platform.assets().setPathPrefix("WebSA_WS12/");
-        PlayN.run(injector.createApp());
+    public void onModuleLoad() {
+        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
+
 }
