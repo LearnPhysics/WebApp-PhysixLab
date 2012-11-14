73b53e239a5eecb623d7b542be43433e436d527e
Daniel Heinrich <dannynullzwo@gmail.com>
2012-11-14 11:00:44 +0100

    twitter login

--- html/src/main/java/de/hofuniversity/iws/html/WebAppHtml.java
+++ html/src/main/java/de/hofuniversity/iws/html/WebAppHtml.java
@@ -6,11 +6,9 @@ import com.google.gwt.core.client.GWT;
 public class WebAppHtml implements EntryPoint {
 
     private final WebAppInjector injector = GWT.create(WebAppInjector.class);
-    
+
     @Override
     public void onModuleLoad() {
         throw new UnsupportedOperationException("Not supported yet.");
-        
     }
-
 }
