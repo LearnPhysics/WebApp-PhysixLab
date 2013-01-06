7dc3ac3bf155ebff0beb0963f32f3b8eeda9a50c
Daniel Heinrich <dannynullzwo@gmail.com>
2013-01-06 02:15:27 +0100

    bugfix for generator

--- GameGenerator/src/main/java/de/hofuniversity/iws/gamegenerator/GameInstantiatorGenerator.java
+++ GameGenerator/src/main/java/de/hofuniversity/iws/gamegenerator/GameInstantiatorGenerator.java
@@ -10,57 +10,57 @@ import java.util.*;
 import com.google.gwt.core.ext.*;
 import com.google.gwt.core.ext.typeinfo.*;
 import com.google.gwt.user.rebind.*;
-import de.hofuniversity.iws.client.playn.*;
 
 /**
  *
  * @author some
  */
-public class GameInstantiatorGenerator extends Generator  {
-
+public class GameInstantiatorGenerator extends Generator {
+    
     @Override
     public String generate(TreeLogger logger, GeneratorContext context, String typeName) throws UnableToCompleteException {
         TypeOracle oracle = context.getTypeOracle();
-
-        JClassType instantiableType = oracle.findType(PhysicGame.class.getName());
-
+        
+        JClassType instantiableType = oracle.findType("de.hofuniversity.iws.client.playn.PhysicGame");
+        
         List<JClassType> clazzes = new ArrayList<JClassType>();
         for (JClassType classType : oracle.getTypes()) {
             if (!classType.equals(instantiableType) && classType.isAssignableTo(instantiableType)) {
                 clazzes.add(classType);
             }
         }
-
+        
         final String genPackageName = "de.hofuniversity.iws.client.play";
         final String genClassName = "GameInstantiatorImpl";
-
+        
         ClassSourceFileComposerFactory composer = new ClassSourceFileComposerFactory(genPackageName, genClassName);
-        composer.addImplementedInterface(GameInstantiator.class.getCanonicalName());
-
+        composer.addImplementedInterface("de.hofuniversity.iws.client.playn.GameInstantiator");
+        composer.addImport("de.hofuniversity.iws.client.playn.PhysicGame");
+        
         PrintWriter printWriter = context.tryCreate(logger, genPackageName, genClassName);
-
+        
         if (printWriter != null) {
             SourceWriter sourceWriter = composer.createSourceWriter(context, printWriter);
             sourceWriter.println("GameInstantiatorImpl( ) {");
             sourceWriter.println("}");
-
+            
             printFactoryMethod(clazzes, sourceWriter);
-
+            
             sourceWriter.commit(logger);
         }
         return composer.getCreatedClassName();
     }
-
+    
     private void printFactoryMethod(List<JClassType> clazzes, SourceWriter sourceWriter) {
         sourceWriter.println();
-
+        
         sourceWriter.println("public PhysicGame createGame(String clazz) {");
-
+        
         for (JClassType classType : clazzes) {
             if (classType.isAbstract()) {
                 continue;
             }
-
+            
             sourceWriter.println();
             sourceWriter.indent();
             sourceWriter.println("if (\"" + classType.getQualifiedSourceName() + "\".equals(clazz)) {");
